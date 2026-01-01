510bed9fdbac964e8190fc4925e4f39975767590
KOWX712 <leecc0503@gmail.com>
2026-01-02 00:57:09 +0800

    scripts/customize: use package_config ftom ap

--- module/customize.sh
+++ module/customize.sh
@@ -5,3 +5,8 @@ fi
 set_perm_recursive "$MODPATH/bin" 0 2000 0755 0755
 
 mkdir -p /data/adb/kp-next
+
+# try get package_config from APatch
+if [ -f "/data/adb/ap/package_config" ] && [ ! -f "/data/adb/kp-next/package_config" ]; then
+    cp "/data/adb/ap/package_config" /data/adb/kp-next/package_config
+fi
